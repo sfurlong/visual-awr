445924ae9271c41deaab355a48dd844687515553
sfurlong <steveinvienna@gmail.com>
2015-05-06 22:36:36 -0400

    Added Apache Open source License

--- src/org/altaprise/vawr/charts/demos/StackedBarChartDemo4.java
+++ src/org/altaprise/vawr/charts/demos/StackedBarChartDemo4.java
@@ -1,43 +1,21 @@
 package org.altaprise.vawr.charts.demos;
 
-/* ===========================================================
- * JFreeChart : a free chart library for the Java(tm) platform
- * ===========================================================
+/*******************************************************************************
  *
- * (C) Copyright 2000-2004, by Object Refinery Limited and Contributors.
+ * Copyright 2015 Stephen Furlong
  *
- * Project Info:  http://www.jfree.org/jfreechart/index.html
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This library is free software; you can redistribute it and/or modify it under the terms
- * of the GNU Lesser General Public License as published by the Free Software Foundation;
- * either version 2.1 of the License, or (at your option) any later version.
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License along with this
- * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA 02111-1307, USA.
- *
- * [Java is a trademark or registered trademark of Sun Microsystems, Inc. 
- * in the United States and other countries.]
- *
- * -------------------------
- * StackedBarChartDemo4.java
- * -------------------------
- * (C) Copyright 2004, by Object Refinery Limited and Contributors.
- *
- * Original Author:  David Gilbert (for Object Refinery Limited);
- * Contributor(s):   -;
- *
- * $Id: StackedBarChartDemo4.java,v 1.6 2004/05/12 16:01:58 mungady Exp $
- *
- * Changes
- * -------
- * 29-Apr-2004 : Version 1 (DG);
- *
- */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 
 import java.awt.Color;
 import java.awt.GradientPaint;
