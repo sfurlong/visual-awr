5f62c6e7167dbdb2d31195c3c792e3b5aa1d4ff0
Furlong <furlons@784f43a651b5.ant.amazon.com>
2017-09-02 16:42:06 -0400

    added

---
+++ runjunit.bat
@@ -0,0 +1,30 @@
+@echo off
+@setlocal
+REM This Batch file is used to set all the environment files necessary to 
+REM ###########################################
+REM ####  SET THESE TO YOUR WIN32 ENV SETTINGS
+REM ###########################################
+set APP_HOME=.
+REM set JAVA_HOME=D:\oracle\Java\jdk1.7.0_40
+set JAVA_OPTS=-Xms64M -Xmx1024M
+REM set JAVA_OPTS=%JAVA_OPTS% -DDEBUG
+REM ###########################################
+
+
+set CLASSPATH=
+
+set CLASSPATH=%APP_HOME%\lib\jcommon-1.0.22.jar;%CLASSPATH%
+set CLASSPATH=%APP_HOME%\lib\jfreechart-1.0.17.jar;%CLASSPATH%
+set CLASSPATH=%APP_HOME%\lib\vawr.jar;%CLASSPATH%
+set CLASSPATH=%APP_HOME%\lib\vawr-o.jar;%CLASSPATH%
+set CLASSPATH=%APP_HOME%\lib\vawr-utils.jar;%CLASSPATH%
+set CLASSPATH=%APP_HOME%\lib\ojdbc5.jar;%CLASSPATH%
+set CLASSPATH=%APP_HOME%\lib\junit-4.12.jar;%CLASSPATH%
+set CLASSPATH=%APP_HOME%\lib\hamcrest-junit-2.0.0.0.jar;%CLASSPATH%
+set CLASSPATH=%APP_HOME%\lib\java-hamcrest-2.0.0.0.jar;%CLASSPATH%
+
+
+
+set path=%JAVA_HOME%\bin;%path%
+
+java %JAVA_OPTS% -DAPP_HOME=%APP_HOME% org.junit.runner.JUnitCore org.altaprise.test.TestDemoFilesAndCharts > junit.out 2>&1
